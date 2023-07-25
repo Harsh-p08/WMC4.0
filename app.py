b8216be56f20fe5499333ac8606b98a48420ae19
Harsh-p08 <harsh.p8@ahduni.edu.in>
2023-07-25 23:18:33 +0530

    Flask Tried

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("LP_index.html")
 
+@app.route('/SP_index.html')
+def hello_signup():
+    return render_template("SP_index.html")
+
 if __name__=="__main__":
     app.run(debug=True)

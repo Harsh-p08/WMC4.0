08fb43f1c6b372a09e506da47250907ae985eec8
Harsh-p08 <harsh.p8@ahduni.edu.in>
2023-07-28 22:54:10 +0530

    Message Stored and displayed in VScode

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def hello_world():
+    if request.method == "POST":
+        entry_username = request.form.get("username")
+        entry_password= request.form.get("password")
+        print(entry_username , entry_password )
     return render_template("LP_index.html")
 
-@app.route('/SP_index.html')
+@app.route('/SP_index.html', methods=["GET","POST"])
 def hello_signup():
     return render_template("SP_index.html")
 

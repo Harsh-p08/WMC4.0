5a75c0edeef4101575b9cb6714e87fb34080aeaf
Harsh-p08 <harsh.p8@ahduni.edu.in>
2023-07-30 21:46:18 +0530

    Mongodb Updated

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template,request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient("mongodb+srv://harshp8:Harsh6204@wmc4-0.efyrnjc.mongodb.net/")
+app.db = client.WMC4
+
+entries=[]
 
 @app.route('/', methods=["GET","POST"])
 def hello_world():
     if request.method == "POST":
         entry_username = request.form.get("username")
         entry_password= request.form.get("password")
-        print(entry_username , entry_password )
+        app.db.entries.insert_one({"username1" : entry_username , "password1" : entry_password })
     return render_template("LP_index.html")
+
 
 @app.route('/SP_index.html', methods=["GET","POST"])
 def hello_signup():
